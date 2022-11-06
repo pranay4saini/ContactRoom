64bb61aaca7b60c65b093d9c357a71b55b2e0b3a
pranay <pranay4saini@gmail.com>
2022-11-06 17:37:16 +0530

    added Recycler View

--- app/src/main/java/com/pranay/contactroom/MainActivity.java
+++ app/src/main/java/com/pranay/contactroom/MainActivity.java
@@ -7,17 +7,27 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pranay.contactroom.adapter.RecyclerViewAdapter;
 import com.pranay.contactroom.databinding.ActivityMainBinding;
 import com.pranay.contactroom.model.Contact;
 import com.pranay.contactroom.model.ContactViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NEW_CONTACT_ACTIVITY_REQUEST_CODE = 1;
     private ActivityMainBinding binding;
     private TextView textView;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+//    private LiveData<List<Contact>> contactList;
+
 
 
     private ContactViewModel contactViewModel;
@@ -26,23 +36,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.textView);
+
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
         contactViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this
                 .getApplication())
                 .create(ContactViewModel.class);
         contactViewModel.getAllContacts().observe(MainActivity.this, contacts -> {
-            StringBuilder builder = new StringBuilder();
-            for (Contact contact : contacts) {
 
-                builder.append(" - ").append(contact.getName()).append(" Occupation: ").append(contact.getOccupation());
-                Log.d("TAG", "onCreate: " + contact.getName());
-            }
-           textView.setText(builder.toString());
+            //Set up adapter
+            recyclerViewAdapter = new RecyclerViewAdapter(contacts,MainActivity.this);
+            recyclerView.setAdapter(recyclerViewAdapter);
 
 
         });
+
 
         FloatingActionButton fab = findViewById(R.id.add_contact_fab);
 
---
+++ app/src/main/java/com/pranay/contactroom/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.pranay.contactroom.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pranay.contactroom.R;
+import com.pranay.contactroom.model.Contact;
+
+import java.util.List;
+import java.util.Objects;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private List<Contact> contactList;
+    private Context context;
+
+    public RecyclerViewAdapter(List<Contact> contactList, Context context) {
+        this.contactList = contactList;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contact_row,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Contact contact = Objects.requireNonNull(contactList.get(position));
+        holder.name.setText(contact.getName());
+        holder.occupation.setText(contact.getOccupation());
+    }
+
+    @Override
+    public int getItemCount() {
+        return Objects.requireNonNull(contactList).size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView name;
+            public TextView occupation;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.row_name_textview);
+            occupation = itemView.findViewById(R.id.row_ocupation_textview);
+        }
+    }
+}
